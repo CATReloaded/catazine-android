11d19c379a66071399e7eedb78d6c391b230ec20
ahmed_rashwan <ahmed.t.rashwan@gmail.com>
2015-09-08 23:15:31 +0200

    Add requestArticleById in NetworkHelper

--- app/src/main/java/net/catazine/live/MainActivity.java
+++ app/src/main/java/net/catazine/live/MainActivity.java
@@ -1,19 +1,17 @@
 package net.catazine.live;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.app.ActionBar;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.support.v4.widget.DrawerLayout;
-
-import net.catazine.live.Utility.NetworkHelper;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationDrawerFragment.NavigationDrawerCallbacks {
@@ -38,7 +36,7 @@ public class MainActivity extends AppCompatActivity
                 R.id.navigation_drawer,
                 (DrawerLayout) findViewById(R.id.drawer_layout));
 
-        NetworkHelper.requestArticlesHeaders(1);
+
     }
 
     @Override
